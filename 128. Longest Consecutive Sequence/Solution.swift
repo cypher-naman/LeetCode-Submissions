fe5028ffeea1128a20fd50d2463a0778fba24f50
Naman Sharma <naman123454321@gmail.com>
2024-07-17 11:01:41 +0530

    Swift Solution
            Submission Statistics:
            Question #: 128
            Language: Swift
            Runtime: 237 ms
            Runtime percentile: 76.69
            Memory: 20.232 MB
            Memory percentile: 46.83

--- 128. Longest Consecutive Sequence/Solution.swift
+++ 128. Longest Consecutive Sequence/Solution.swift
@@ -1,24 +1,22 @@
 class Solution {
     func longestConsecutive(_ nums: [Int]) -> Int {
-    let set1 = Set(nums).sorted()
-    if nums.count == 0 {
-        return 0
-    }
-    var highestCount = 1
-    var previous = Int.min
-    var currentHighest = 1
-    for i in set1 {
-        if (previous+1) == i {
-            currentHighest += 1
-            previous += 1
-            if currentHighest > highestCount {
-                highestCount = currentHighest
+        let set = Set(nums)
+        var longestStreak = 0
+
+        for num in set {
+            if !set.contains(num - 1) {
+                var currentNum = num
+                var currentStreak = 1
+
+                while set.contains(currentNum + 1) {
+                    currentNum += 1
+                    currentStreak += 1
+                }
+
+                longestStreak = max(longestStreak, currentStreak)
             }
-        } else {
-            previous = i
-            currentHighest = 1
         }
+
+        return longestStreak
     }
-    return highestCount
-}
 }
