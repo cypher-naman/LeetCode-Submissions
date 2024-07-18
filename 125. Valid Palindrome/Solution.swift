dd36af536c78c812be5e23f0a00ae51b849b5443
Naman Sharma <naman123454321@gmail.com>
2024-07-18 17:46:10 +0530

    Swift Solution
            Submission Statistics:
            Question #: 125
            Language: Swift
            Runtime: 23 ms
            Runtime percentile: 59.38
            Memory: 18.848 MB
            Memory percentile: 16.89

--- 125. Valid Palindrome/Solution.swift
+++ 125. Valid Palindrome/Solution.swift
@@ -1,31 +1,10 @@
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
-    let string: String = s.lowercased()
-    let charSet = Set("abcdefghijklmnopqrstuvwxyz")
-    let numberSet = Set(0...9)
-    //let charSet = Set((97...122).map { Character(UnicodeScalar($0)!)})
-    var newString = string.filter({(charSet.contains($0) || numberSet.contains($0.wholeNumberValue ?? -1))})
+    var newString = Array(s.lowercased().filter { $0.isLetter || $0.isNumber })
     
-    if newString == "" || newString.count == 1 {
-            return true
+    if newString.isEmpty || newString.count == 1 {
+        return true
     }
-  
-    // using this as causing time exceed in submission
-    return newString == String(newString.reversed())
-//
-//    for i in stride(from: 0, to: loopEnd, by: 1) {
-//        if let index1 = newString.index(newString.startIndex,
-//                                        offsetBy: i,
-//                                        limitedBy: newString.endIndex),
-//           let index2 = newString.index(newString.startIndex,
-//                                        offsetBy: ((newString.count-1) - i),
-//                                        limitedBy: newString.endIndex) {
-//            
-//            if newString[index1] != newString[index2] {
-//                return false
-//            }
-//        }
-//    }
-//    return true
-}
+    return newString == newString.reversed()
+    }
 }
