ae3b940085bf81c9b7110ae9a1fe2263c04c3a56
Naman Sharma <naman123454321@gmail.com>
2024-09-06 21:22:03 +0530

    C++ Solution
            Submission Statistics:
            Question #: 2006
            Language: C++
            Runtime: 96 ms
            Runtime percentile: 52.08
            Memory: 77.14 MB
            Memory percentile: 16.68

--- 2006. Find the Student that Will Replace the Chalk/Solution.cpp
+++ 2006. Find the Student that Will Replace the Chalk/Solution.cpp
@@ -3,23 +3,25 @@ public:
     int chalkReplacer(vector<int>& chalk, int k) {
 
         long long consumption = 0;
-        int students = chalk.size()-1;
-        map<int,int> mp;
+        // int students = chalk.size()-1;
+        // map<int,int> mp;
         for(int i=0; i<chalk.size(); i++) {
             consumption+=chalk[i];
-            mp[i] = consumption;
+            // mp[i] = consumption;
         }
         int newK = k;
-        if (k > consumption) {
-            newK = k%consumption;
-        } 
+        newK = k%consumption;
+        consumption = 0;
+
         int returnIndex = 0;
         for(int i=0; i<chalk.size(); i++) {
             // cout<<mp[i]<<endl;
+            consumption += chalk[i];
+
             if (newK == 0) {
                 returnIndex = 0;
                 break;
-            } else if (mp[i] > newK) {
+            } else if (consumption > newK) {
                 returnIndex = i;
                 break;
             }
